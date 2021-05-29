487bf350842aea28076743b39bacd24fc424ddc9
unknown <agustin.dugo13@gmail.com>
2021-05-29 14:56:11 -0300

    first commit

---
+++ JS/menu.js
@@ -0,0 +1,6 @@
+let botonMenu = document.getElementById("boton-menu");
+let menu =document.getElementById("menu");
+
+botonMenu.addEventListener("click", function() {
+    menu.classList.toggle("mostrar");    
+}); 
